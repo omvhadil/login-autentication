e04d80aa527367c9855ee42ee2417ea7fd59b8b2
omvhadil <fadil.rohman.327@gmail.com>
2023-05-28 12:32:03 +0700

    include

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,11 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('includes._style')
     <title>Document</title>
 </head>
 
 <body>
     <h5>Dashboard</h5>
+
+    @include('includes._script')
 </body>
 
 </html>
