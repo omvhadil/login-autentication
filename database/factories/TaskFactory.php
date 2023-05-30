a3345ec80646d14d4fc1303804ee003c8ee906b5
omvhadil <fadil.rohman.327@gmail.com>
2023-05-30 07:26:19 +0700

    task

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Task;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Task::class;
+
+    public function definition(): array
+    {
+        return [
+            'list' => $this->faker->sentence(),
+            'mark' => rand(0, 1),
+
+        ];
+    }
+}
